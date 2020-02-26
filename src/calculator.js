092135a39caa70f58e9a62b98908ee5d09b44187
rokoc003 <roxane.kocem@yahoo.de>
2020-02-26 10:57:37 +0100

    Add calculator functions

--- src/calculator.js
+++ src/calculator.js
@@ -1,1 +1,20 @@
-document.querySelector(".calculator__output").value = "42";
+"use strict";
+
+const calculatorOutput = document.querySelector(".calculator__output");
+calculatorOutput.value = add(3, 2);
+
+function add(numberOne, numberTwo) {
+  return numberOne + numberTwo;
+}
+
+function subtract(numberOne, numberTwo) {
+  return numberOne - numberTwo;
+}
+
+function divide(numberOne, numberTwo) {
+  return numberOne / numberTwo;
+}
+
+function multiply(numberOne, numberTwo) {
+  return numberOne * numberTwo;
+}
