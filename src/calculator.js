5a09388bfc79a57bb6038090f574f5efb498cf4a
rokoc003 <roxane.kocem@yahoo.de>
2020-02-26 14:29:42 +0100

    Clear functionality

--- src/calculator.js
+++ src/calculator.js
@@ -3,6 +3,7 @@
 const calculatorOutput = document.querySelector(".calculator__output");
 const calculatorInputs = document.querySelectorAll(".calculator__input");
 const calculatorResult = document.querySelector(".calculator__result");
+const calculatorClearAll = document.querySelector(".calculator__clearall");
 
 function add(numberOne, numberTwo) {
   return numberOne + numberTwo;
@@ -26,6 +27,14 @@ const numberTwo = Number(calculatorInputs[3].innerText);
 function handleClick() {
   calculatorOutput.value = add(numberOne, numberTwo);
   console.log("clicked");
+  //console.log(calculatorOutput.value);
 }
 
 calculatorResult.addEventListener("click", handleClick);
+
+function allclearClick() {
+  calculatorOutput.value = "";
+  console.log("click");
+}
+
+calculatorClearAll.addEventListener("click", allclearClick);
